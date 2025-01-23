ec28ec8c844d70b8c1e5e9564b1a0896d21fd5b7
lurodrisilva <lurodrisilva@hotmail.com>
2025-01-22 23:13:58 -0300

    adding logger

--- debug.go
+++ debug.go
@@ -6,6 +6,9 @@ import (
 	"runtime"
 	"runtime/debug"
 	globaldebug "runtime/debug"
+
+	"github.com/DataDog/appsec-internal-go/log"
+	"github.com/go-logr/logr"
 )
 
 // DebugOptions is a type for debug options
@@ -103,6 +106,97 @@ func PrintDebug(message string, options DebugOptions) error {
 	}
 
 	fmt.Println("\nFinish debug...")
+
+	return nil
+}
+
+// PrintDebug prints debug information
+func PrintDebugWithLog(message string, options DebugOptions, logger logr.Logger) error {
+
+	var level = options.Level
+
+	// If Debug is disabled, return
+	if !options.Enabled {
+		return nil
+	}
+
+	// Default Level
+	if !isValidOption(level) {
+		return errors.New("invalid debug option: " + level)
+	}
+
+	// Read GC Stats
+	var gcStats globaldebug.GCStats
+	globaldebug.ReadGCStats(&gcStats)
+
+	// Read Mem Stats
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	// Read Build Info
+	buildInfo, ok := globaldebug.ReadBuildInfo()
+
+	// // Initial Messages
+	// fmt.Println("\nStart debug...")
+
+	// Business Message
+	logger.Info(message)
+
+	// Stack Trace
+	if level == STACK || level == ALL {
+		stack := fmt.Sprintf("Stack Trace:%s", debug.Stack())
+		log.Info(stack)
+	}
+
+	// Mem Stats
+	if level == MEM || level == ALL {
+		message_level := fmt.Sprintf("Alloc: %d bytes", memStats.Alloc)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("TotalAlloc: %d bytes", memStats.TotalAlloc)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapAlloc: %d bytes", memStats.HeapAlloc)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapSys: %d bytes", memStats.HeapSys)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapIdle: %d bytes", memStats.HeapIdle)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapInuse: %d bytes", memStats.HeapInuse)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapReleased: %d bytes", memStats.HeapReleased)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("HeapObjects: %d", memStats.HeapObjects)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("StackInUse: %d bytes", memStats.StackInuse)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("StackSys: %d bytes", memStats.StackSys)
+		log.Info(message_level)
+		message_level = fmt.Sprintf("NumGC: %d", memStats.NumGC)
+		log.Info(message_level)
+	}
+
+	// GC Stats
+	if level == GC || level == ALL {
+		message_gc := fmt.Sprintf("LastGC: %v", gcStats.LastGC)
+		log.Info(message_gc)
+		message_gc = fmt.Sprintf("NumGC: %v", gcStats.NumGC)
+		log.Info(message_gc)
+		message_gc = fmt.Sprintf("PauseTotal: %v", gcStats.PauseTotal)
+		log.Info(message_gc)
+		message_gc = fmt.Sprintf("Pause: %v", gcStats.Pause)
+		log.Info(message_gc)
+		message_gc = fmt.Sprintf("PauseEnd: %v", gcStats.PauseEnd)
+		log.Info(message_gc)
+		message_gc = fmt.Sprintf("PauseQuantiles: %v", gcStats.PauseQuantiles)
+		log.Info(message_gc)
+	}
+
+	// Build Info
+	if level == BUILD || level == ALL {
+		if ok {
+			message_build := fmt.Sprintf("Build Info: %s", buildInfo)
+			log.Info(message_build)
+		}
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/luciano-personal-org/debug
 
 go 1.23.3
+
+require github.com/go-logr/logr v1.4.2 // indirect
