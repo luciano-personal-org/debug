99288fde5fabebd53864040d4969a34b88619f0f
lurodrisilva <lurodrisilva@hotmail.com>
2025-01-22 23:56:32 -0300

    fixing bug

--- debug.go
+++ debug.go
@@ -112,91 +112,57 @@ func PrintDebug(message string, options DebugOptions) error {
 
 // PrintDebug prints debug information
 func PrintDebugWithLog(message string, options DebugOptions, logger logr.Logger) error {
-
 	var level = options.Level
-
 	// If Debug is disabled, return
 	if !options.Enabled {
 		return nil
 	}
-
 	// Default Level
 	if !isValidOption(level) {
 		return errors.New("invalid debug option: " + level)
 	}
-
 	// Read GC Stats
 	var gcStats globaldebug.GCStats
 	globaldebug.ReadGCStats(&gcStats)
-
 	// Read Mem Stats
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
-
 	// Read Build Info
 	buildInfo, ok := globaldebug.ReadBuildInfo()
-
-	// // Initial Messages
-	// fmt.Println("\nStart debug...")
-
 	// Business Message
 	logger.WithCallDepth(2).Info(message)
-
 	// Stack Trace
 	if level == STACK || level == ALL {
-		stack := fmt.Sprintf("Stack Trace:%s", debug.Stack())
-		logger.WithCallDepth(2).Info(stack)
+		logger.WithCallDepth(2).Info(fmt.Sprintf("Stack Trace:%s", debug.Stack()))
 	}
-
 	// Mem Stats
 	if level == MEM || level == ALL {
-		message_level := fmt.Sprintf("Alloc: %d bytes", memStats.Alloc)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("TotalAlloc: %d bytes", memStats.TotalAlloc)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapAlloc: %d bytes", memStats.HeapAlloc)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapSys: %d bytes", memStats.HeapSys)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapIdle: %d bytes", memStats.HeapIdle)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapInuse: %d bytes", memStats.HeapInuse)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapReleased: %d bytes", memStats.HeapReleased)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("HeapObjects: %d", memStats.HeapObjects)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("StackInUse: %d bytes", memStats.StackInuse)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("StackSys: %d bytes", memStats.StackSys)
-		logger.WithCallDepth(2).Info(message_level)
-		message_level = fmt.Sprintf("NumGC: %d", memStats.NumGC)
-		logger.WithCallDepth(2).Info(message_level)
+		logger.WithCallDepth(2).Info(fmt.Sprintf("Alloc: %d bytes", memStats.Alloc))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("TotalAlloc: %d bytes", memStats.TotalAlloc))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapAlloc: %d bytes", memStats.HeapAlloc))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapSys: %d bytes", memStats.HeapSys))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapIdle: %d bytes", memStats.HeapIdle))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapInuse: %d bytes", memStats.HeapInuse))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapReleased: %d bytes", memStats.HeapReleased))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("HeapObjects: %d", memStats.HeapObjects))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("StackInUse: %d bytes", memStats.StackInuse))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("StackSys: %d bytes", memStats.StackSys))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("NumGC: %d", memStats.NumGC))
 	}
-
 	// GC Stats
 	if level == GC || level == ALL {
-		message_gc := fmt.Sprintf("LastGC: %v", gcStats.LastGC)
-		logger.WithCallDepth(2).Info(message_gc)
-		message_gc = fmt.Sprintf("NumGC: %v", gcStats.NumGC)
-		logger.WithCallDepth(2).Info(message_gc)
-		message_gc = fmt.Sprintf("PauseTotal: %v", gcStats.PauseTotal)
-		logger.WithCallDepth(2).Info(message_gc)
-		message_gc = fmt.Sprintf("Pause: %v", gcStats.Pause)
-		logger.WithCallDepth(2).Info(message_gc)
-		message_gc = fmt.Sprintf("PauseEnd: %v", gcStats.PauseEnd)
-		logger.WithCallDepth(2).Info(message_gc)
-		message_gc = fmt.Sprintf("PauseQuantiles: %v", gcStats.PauseQuantiles)
-		logger.WithCallDepth(2).Info(message_gc)
+		logger.WithCallDepth(2).Info(fmt.Sprintf("LastGC: %v", gcStats.LastGC))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("NumGC: %v", gcStats.NumGC))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("PauseTotal: %v", gcStats.PauseTotal))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("Pause: %v", gcStats.Pause))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("PauseEnd: %v", gcStats.PauseEnd))
+		logger.WithCallDepth(2).Info(fmt.Sprintf("PauseQuantiles: %v", gcStats.PauseQuantiles))
 	}
-
 	// Build Info
 	if level == BUILD || level == ALL {
 		if ok {
-			message_build := fmt.Sprintf("Build Info: %s", buildInfo)
-			logger.WithCallDepth(2).Info(message_build)
+			logger.WithCallDepth(2).Info(fmt.Sprintf("Build Info: %s", buildInfo))
 		}
 	}
-
 	return nil
 }
